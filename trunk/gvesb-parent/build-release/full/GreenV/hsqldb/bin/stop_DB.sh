740a2cf74be7868fa3844e98638240e44a7f0128
dmg75it <dmg75it@a465400c-843c-436a-95fd-a8af927e9836>
2013-01-07 16:39:52 +0000

    improved packages generation; added package Full generation

--- trunk/gvesb-parent/build-release/full/GreenV/hsqldb/bin/stop_DB.sh
+++ trunk/gvesb-parent/build-release/full/GreenV/hsqldb/bin/stop_DB.sh
@@ -7,3 +7,4 @@ export HSQLDB_HOME=$GV_HOME/hsqldb
 export HSQTOOL=$HSQLDB_HOME/lib/sqltool.jar
 
 $JAVA_HOME/bin/java -jar $HSQTOOL  --inlineRc=url=jdbc:hsqldb:hsql://localhost:9001/gvesb,user=SA,password= --sql="shutdown;"
+$JAVA_HOME/bin/java -jar $HSQTOOL  --inlineRc=url=jdbc:hsqldb:hsql://localhost:9001/ode,user=SA,password= --sql="shutdown;"
