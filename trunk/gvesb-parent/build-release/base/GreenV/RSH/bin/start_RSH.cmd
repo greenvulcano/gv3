9e6c04f0025912b1745847169d638818a8830ab3
dmg75it <dmg75it@a465400c-843c-436a-95fd-a8af927e9836>
2013-02-11 09:08:12 +0000

    Corrected RSH module deployment configuration

--- trunk/gvesb-parent/build-release/base/GreenV/RSH/bin/start_RSH.cmd
+++ trunk/gvesb-parent/build-release/base/GreenV/RSH/bin/start_RSH.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 FOR /F "tokens=*" %%B IN ('echo %~dp0') DO SET BASEDIR=%%B
-call "%BASEDIR%"\..\bin\env_scripts.cmd
+call "%BASEDIR%"\..\..\bin\env_scripts.cmd
 
 set RSH_HOME=%GV_HOME%\RSH
 set CLASSPATH=%RSH_HOME%\conf;%RSH_HOME%\lib\gvbase.jar;%RSH_HOME%\lib\gvrsh_commons.jar;%RSH_HOME%\lib\gvrsh_server.jar;%RSH_HOME%\lib\commons-logging.jar;%RSH_HOME%\lib\commons-io.jar;%RSH_HOME%\lib\jaxen.jar;%RSH_HOME%\lib\log4j.jar;%RSH_HOME%\lib\rmiio.jar
