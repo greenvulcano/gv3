4a0eccd7446195f98eff847bb201be0a02e5c4e3
Gianluca <dmg75it@users.noreply.github.com>
2014-05-16 09:08:03 +0000

    Added ADAM adapter skeleton

---
+++ gvesb-parent/gvesb-adapters/gvadam/src/main/java/it/greenvulcano/gvesb/virtual/adam/AdamSearchCallOperation.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2009-2014 GreenVulcano ESB Open Source Project. All rights
+ * reserved.
+ * 
+ * This file is part of GreenVulcano ESB.
+ * 
+ * GreenVulcano ESB is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * GreenVulcano ESB is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GreenVulcano ESB. If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.greenvulcano.gvesb.virtual.adam;
+
+import it.greenvulcano.configuration.XMLConfig;
+import it.greenvulcano.gvesb.buffer.GVBuffer;
+import it.greenvulcano.gvesb.virtual.CallException;
+import it.greenvulcano.gvesb.virtual.CallOperation;
+import it.greenvulcano.gvesb.virtual.ConnectionException;
+import it.greenvulcano.gvesb.virtual.InitializationException;
+import it.greenvulcano.gvesb.virtual.InvalidDataException;
+import it.greenvulcano.gvesb.virtual.OperationKey;
+import it.greenvulcano.log.GVLogger;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Node;
+
+
+/**
+ * 
+ * @version 3.4.0 30/apr/2014
+ * @author GreenVulcano Developer Team
+ */
+public class AdamSearchCallOperation implements CallOperation
+{
+    private static Logger    logger          = GVLogger.getLogger(AdamSearchCallOperation.class);
+
+    private OperationKey     key             = null;
+    private String           host            = null;
+    private int              port            = -1;
+    private int              timeout         = 10000;
+
+
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see it.greenvulcano.gvesb.virtual.Operation#init(org.w3c.dom.Node)
+     */
+    @Override
+    public void init(Node node) throws InitializationException
+    {
+        logger.debug("Init start");
+        try {
+            host = XMLConfig.get(node, "@host");
+            port = XMLConfig.getInteger(node, "@port");
+            timeout = XMLConfig.getInteger(node, "@timeout", 10) * 1000;
+
+            logger.debug("init - loaded parameters: host = " + host + " - port: " + port + " - timeout: " + timeout);
+
+
+            logger.debug("Init stop");
+        }
+        catch (Exception exc) {
+            throw new InitializationException("GV_INIT_SERVICE_ERROR", new String[][]{{"message", exc.getMessage()}},
+                    exc);
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * it.greenvulcano.gvesb.virtual.CallOperation#perform(it.greenvulcano.gvesb
+     * .buffer.GVBuffer)
+     */
+    @Override
+    public GVBuffer perform(GVBuffer gvBuffer) throws ConnectionException, CallException, InvalidDataException
+    {
+        try {
+            Object input = gvBuffer.getObject();
+            if (input instanceof String) {
+                logger.debug("Input object is a String");
+
+            }
+            else if (input instanceof byte[]) {
+                logger.debug("Input object is a byte array");
+
+            }
+            else if (input instanceof Node) {
+                logger.debug("Input object is a Node");
+
+            }
+            else {
+                throw new Exception("Invalid input type: " + input.getClass());
+            }
+
+            Object out = null;
+
+            gvBuffer.setObject(out);
+        }
+        catch (Exception exc) {
+            throw new CallException("GV_CALL_SERVICE_ERROR", new String[][]{{"service", gvBuffer.getService()},
+                    {"system", gvBuffer.getSystem()}, {"tid", gvBuffer.getId().toString()},
+                    {"message", exc.getMessage()}}, exc);
+        }
+        return gvBuffer;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see it.greenvulcano.gvesb.virtual.Operation#cleanUp()
+     */
+    @Override
+    public void cleanUp()
+    {
+        // do nothing
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see it.greenvulcano.gvesb.virtual.Operation#destroy()
+     */
+    @Override
+    public void destroy()
+    {
+        // do nothing
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * it.greenvulcano.gvesb.virtual.Operation#getServiceAlias(it.greenvulcano
+     * .gvesb.buffer.GVBuffer)
+     */
+    @Override
+    public String getServiceAlias(GVBuffer gvBuffer)
+    {
+        return gvBuffer.getService();
+    }
+
+
+    /**
+     * @see it.greenvulcano.gvesb.virtual.Operation#setKey(it.greenvulcano.gvesb.virtual.OperationKey)
+     */
+    @Override
+    public void setKey(OperationKey key)
+    {
+        this.key = key;
+    }
+
+    /**
+     * @see it.greenvulcano.gvesb.virtual.Operation#getKey()
+     */
+    @Override
+    public OperationKey getKey()
+    {
+        return key;
+    }
+}
