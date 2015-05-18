49f73198a47e5762f0746cac05d27e7e8502b72f
Gianluca <dmg75it@users.noreply.github.com>
2015-05-18 14:41:11 +0000

    Corrected RMI hostname definition in RSH start script.

--- build-release/base/GreenV/RSH/bin/start_RSH.cmd
+++ build-release/base/GreenV/RSH/bin/start_RSH.cmd
@@ -7,7 +7,7 @@ set RSH_HOME=%GV_HOME%\RSH
 set CLASSPATH=%RSH_HOME%\conf;%RSH_HOME%\lib\gvbase.jar;%RSH_HOME%\lib\gvrsh_commons.jar;%RSH_HOME%\lib\gvrsh_server.jar;%RSH_HOME%\lib\commons-logging.jar;%RSH_HOME%\lib\commons-io.jar;%RSH_HOME%\lib\jaxen.jar;%RSH_HOME%\lib\log4j.jar;%RSH_HOME%\lib\rmiio.jar
 
 set RSH_OPTS=-Drsh.app.home=%RSH_HOME%
-set JAVA_OPTS=-server -Xms128M -Xmx256M -XX:MaxPermSize=64m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled
+set JAVA_OPTS=-server -Xms128M -Xmx256M -XX:MaxPermSize=64m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=localhost
 
 %JAVA_HOME%\bin\java %JAVA_OPTS% -cp %CLASSPATH% %RSH_OPTS% it.greenvulcano.gvesb.rsh.server.RSHServer -p 3099
 
