8a3ea07f4f95ebbfd4b85d36f4894b38c7ea946c
Gianluca <dmg75it@users.noreply.github.com>
2015-12-10 16:43:57 +0000

    Minor changes

--- gvesb-parent/gvesb/test-resources/scripts/js/initscope.js
+++ gvesb-parent/gvesb/test-resources/scripts/js/initscope.js
@@ -1,3 +1,4 @@
+load("nashorn:mozilla_compat.js");
 importPackage(Packages.it.greenvulcano.gvesb.buffer);
 importPackage(Packages.it.greenvulcano.gvesb.utils);
 importPackage(Packages.it.greenvulcano.util.xml);
