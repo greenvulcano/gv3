531ed095ac6f6c66f0a676a7b7eec91778e3c7d6
Gianluca <dmg75it@users.noreply.github.com>
2015-12-10 16:42:29 +0000

    Minor changes

--- gvesb-parent/gvesb-vcl/gvvcl-file/src/test/java/tests/unit/vcl/file/remote/RemoteManagerTestCase.java
+++ gvesb-parent/gvesb-vcl/gvvcl-file/src/test/java/tests/unit/vcl/file/remote/RemoteManagerTestCase.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2009-2010 GreenVulcano ESB Open Source Project. All rights
  * reserved.
- * 
+ *
  * This file is part of GreenVulcano ESB.
- * 
+ *
  * GreenVulcano ESB is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * GreenVulcano ESB is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with GreenVulcano ESB. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -95,7 +95,7 @@ public class RemoteManagerTestCase extends TestCase
         GVBuffer result = rm.perform(gvBuffer);
         System.out.println("testExistFile: " + result.getProperty("GVRM_FOUND_FILES_LIST"));
         assertEquals(2, Integer.parseInt(result.getProperty("GVRM_FOUND_FILES_NUM")));
-        assertEquals("Test0.txt;Test1.txt", result.getProperty("GVRM_FOUND_FILES_LIST"));
+        assertEquals("Test1.txt;Test0.txt", result.getProperty("GVRM_FOUND_FILES_LIST"));
     }
 
     /**
