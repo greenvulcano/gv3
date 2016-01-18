5d53a0c535759a7f151a0ae2f1ab13e9ebcf81ea
Gianluca <dmg75it@users.noreply.github.com>
2016-01-18 18:00:40 +0000

    Added support to Java 8 runtime.

--- build-release/base/GreenV/bin/gvstart.sh
+++ build-release/base/GreenV/bin/gvstart.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname $0)
 
 export JB_OPTS="-Djboss.service.binding.set=$JBOSS_PORTSET -Djboss.partition.name=$GV_SERVER_NAMEPartition -Djboss.bind.address=$JBOSS_BIND_ADDRESS -Dbind.address=localhost -Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=localhost  -Dorg.apache.coyote.USE_CUSTOM_STATUS_MSG_IN_HEADER=true"
 export GV_OPTS="-Dgv.app.home=$GV_HOME -Dit.greenvulcano.util.xpath.search.XPathAPIFactory.cfgFileXPath=GVCore.xml|/GVCore/GVXPath/XPath -Djavax.xml.parsers.DocumentBuilderFactory=org.apache.xerces.jaxp.DocumentBuilderFactoryImpl -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl -Djxl.nogc=true -Dit.greenvulcano.log.db.JDBCAppender.logState=false -Dgv.pop.uidcache.manager=it.greenvulcano.gvesb.virtual.pop.uidcache.MemoryUIDCacheManager -Dit.greenvulcano.util.thread.BaseThread.dumpInstCount=true -Dit.greenvulcano.util.thread.BaseThread.dumpCreateStack=false -Dit.greenvulcano.gvesb.identity.IdentityInfo.debug=true"
-export JAVA_OPTS="-Xms512M -Xmx1024M -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"
+export JAVA_OPTS="-Xms512M -Xmx1024M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"
 
 cp $GV_SERVER/log/server.log $GV_SERVER/log/server.log.`date +%Y%m%d%H%M%S` >> /dev/null 2>&1
 rm -rf $GV_SERVER/work
