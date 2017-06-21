2be0fdd3cb4d9f0ec22a12ae04e01588c927a298
Gianluca <dmg75it@users.noreply.github.com>
2017-06-21 10:52:19 +0000

    The dump-in-out attribute, if set on WF nodes, force the logger level to DEBUG

--- gvesb-core/gvcore/src/main/java/it/greenvulcano/gvesb/core/flow/IteratorController.java
+++ gvesb-core/gvcore/src/main/java/it/greenvulcano/gvesb/core/flow/IteratorController.java
@@ -522,6 +522,7 @@ public class IteratorController
                     dataProviderManager.releaseDataProvider(call_dp, dataProvider);
                 }
             }
+            Level level = null;
             NMDC.push();
             try {
                 gvContext.push();
@@ -529,9 +530,11 @@ public class IteratorController
                     NMDC.setOperation(localSubFlow);
                     GVBufferMDC.put(internalGVBuffer);
                 }
+                level = GVLogger.setThreadMasterLevel(subFlow.getLoggerLevel());
                 result = subFlow.perform(internalGVBuffer, onDebug);
             }
             finally {
+                GVLogger.removeThreadMasterLevel(level);
                 NMDC.pop();
                 try {
                     gvContext.pop();
