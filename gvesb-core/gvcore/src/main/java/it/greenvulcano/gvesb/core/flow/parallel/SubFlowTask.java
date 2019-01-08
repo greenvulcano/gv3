b120c4e381c8ae30b86b659aa2d87f360d5a7b92
Gianluca <dmg75it@users.noreply.github.com>
2019-01-08 08:28:18 +0000

    Build package updated to 3.4.0.11

---
+++ gvesb-core/gvcore/src/main/java/it/greenvulcano/gvesb/core/flow/parallel/SubFlowTask.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2009-2013 GreenVulcano ESB Open Source Project. All rights
+ * reserved.
+ * 
+ * This file is part of GreenVulcano ESB.
+ * 
+ * GreenVulcano ESB is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * GreenVulcano ESB is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GreenVulcano ESB. If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.greenvulcano.gvesb.core.flow.parallel;
+
+import it.greenvulcano.gvesb.buffer.GVBuffer;
+import it.greenvulcano.gvesb.core.flow.GVSubFlow;
+import it.greenvulcano.gvesb.gvdp.DataProviderManager;
+import it.greenvulcano.gvesb.gvdp.IDataProvider;
+import it.greenvulcano.gvesb.log.GVBufferMDC;
+import it.greenvulcano.log.GVLogger;
+import it.greenvulcano.log.NMDC;
+import it.greenvulcano.util.thread.ThreadMap;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @version 3.4.0 Jun 17, 2013
+ * @author GreenVulcano Developer Team
+ * 
+ */
+public class SubFlowTask implements Callable<Result>
+{
+    private static final Logger logger            = GVLogger.getLogger(SubFlowTask.class);
+
+    private GVSubFlowPool       pool             = null;
+    private GVSubFlow           subFlow          = null;
+    private GVBuffer            input;
+    private boolean             onDebug;
+    private boolean             changeLogContext;
+    private Map<String, String> logContext;
+    private String              inputRefDP;
+    private boolean             spawned           = false;
+    private String              spawnedName       = null;
+    private boolean             needsOutput       = true;
+
+    public SubFlowTask(GVSubFlowPool pool, GVBuffer input, boolean onDebug, boolean changeLogContext, Map<String, String> logContext, String inputRefDP, boolean needsOutput) {
+        this.pool = pool;
+        this.input = input;
+        this.onDebug = onDebug;
+        this.logContext = logContext;
+        this.changeLogContext = changeLogContext;
+        this.inputRefDP = inputRefDP;
+        this.needsOutput = needsOutput;
+    }
+
+    public SubFlowTask(GVSubFlow subFlow, GVBuffer input, boolean onDebug, boolean changeLogContext, Map<String, String> logContext, String inputRefDP, boolean needsOutput) {
+        this.subFlow = subFlow;
+        this.input = input;
+        this.onDebug = onDebug;
+        this.logContext = logContext;
+        this.changeLogContext = changeLogContext;
+        this.inputRefDP = inputRefDP;
+        this.needsOutput = needsOutput;
+    }
+
+    public void setSpawned(boolean spawned) {
+        this.spawned = spawned;
+    }
+
+    public void setSpawnedName(String spawnedName) {
+        this.spawnedName = spawnedName;
+    }
+
+    @Override
+    public Result call() throws Exception {
+        try {
+            NMDC.push();
+            if (spawned && (spawnedName != null)) {
+                Thread th = Thread.currentThread();
+                String thn = th.getName();
+                thn = thn.substring(thn.lastIndexOf("_"));
+                th.setName(spawnedName + thn);
+            }
+            NMDC.setCurrentContext(logContext);
+
+            Result result = null;
+            GVSubFlow currSubFlow = null;
+            try {
+                GVBuffer internalData = input;
+
+                if (pool != null) {
+                    currSubFlow = pool.getSubFlow();
+                }
+                else {
+                    currSubFlow = subFlow;
+                }
+
+                if (changeLogContext) {
+                    NMDC.setOperation(currSubFlow.getFlowName());
+                    GVBufferMDC.put(internalData);
+                }
+                
+                DataProviderManager dataProviderManager = DataProviderManager.instance();
+                if ((inputRefDP != null) && (inputRefDP.length() > 0)) {
+                    IDataProvider dataProvider = dataProviderManager.getDataProvider(inputRefDP);
+                    try {
+                        logger.debug("Working on Input data provider: " + dataProvider);
+                        internalData = new GVBuffer(input);
+                        dataProvider.setObject(internalData);
+                        Object inputCall = dataProvider.getResult();
+                        internalData.setObject(inputCall);
+                    }
+                    finally {
+                        dataProviderManager.releaseDataProvider(inputRefDP, dataProvider);
+                    }
+                }
+
+                GVBuffer output = currSubFlow.perform(internalData, onDebug);
+                if (needsOutput) {
+                	result = new Result(Result.State.STATE_OK, input, output);
+                }
+                else {
+                	result = new Result(Result.State.STATE_OK);
+                }
+            }
+            catch (InterruptedException exc) {
+                if (spawned) {
+                    logger.error("SubFlow execution interrupted", exc);
+                }
+                if (needsOutput) {
+                    result = new Result(Result.State.STATE_INTERRUPTED, input, exc);
+                }
+                else {
+                    result = new Result(Result.State.STATE_INTERRUPTED, exc);
+                }
+                Thread.currentThread().interrupt();
+            }
+            catch (Exception exc) {
+                if (spawned) {
+                    logger.error("SubFlow execution failed", exc);
+                }
+                if (needsOutput) {
+                    result = new Result(Result.State.STATE_ERROR, input, exc);
+                }
+                else {
+                    result = new Result(Result.State.STATE_ERROR, exc);
+                }
+            }
+            finally {
+                if (pool != null) {
+                    pool.releaseSubFlow(currSubFlow);
+                }
+                if (subFlow != null) {
+                    subFlow.destroy();
+                }
+            }
+            return result;
+        }
+        finally {
+            NMDC.pop();
+            ThreadMap.clean();
+            
+            this.pool = null;
+            this.subFlow = null;
+            //this.input = null;
+            this.logContext = null;
+        }
+    }
+
+    public Result getFailureResult(Throwable cause) {
+        if (needsOutput) {
+        	return new Result(Result.State.STATE_ERROR, input, cause);
+        }
+      	return new Result(Result.State.STATE_ERROR, cause);
+    }
+
+    public Result getTimeoutResult(InterruptedException cause) {
+        if (needsOutput) {
+        	return new Result(Result.State.STATE_ERROR, input, cause);
+        }
+       	return new Result(Result.State.STATE_ERROR, cause);
+    }
+
+    public Result getCancelledResult(CancellationException cause) {
+        if (needsOutput) {
+        	return new Result(Result.State.STATE_CANCELLED, input, cause);
+        }
+        return new Result(Result.State.STATE_CANCELLED, cause);
+    }
+}
--- gvesb-parent/core-libs/gvbase/src/test/resources/scripts/inc/inc.js
+++
@@ -1,4 +0,0 @@
-var services = {"LIST_EXCEL":"1", "LIST_PDF":"1", "LIST_BIRT":"1"};
-function findMatch(svc) {
-	return ("1" == services[svc]);
-}
