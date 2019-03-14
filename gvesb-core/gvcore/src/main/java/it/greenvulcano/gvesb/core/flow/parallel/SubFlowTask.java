ad12d909b5a92e2751d7f4896a1d711c9a392558
Gianluca <dmg75it@users.noreply.github.com>
2019-03-14 11:49:47 +0000

    Improved thread management in Spawn subflow call

--- gvesb-core/gvcore/src/main/java/it/greenvulcano/gvesb/core/flow/parallel/SubFlowTask.java
+++ gvesb-core/gvcore/src/main/java/it/greenvulcano/gvesb/core/flow/parallel/SubFlowTask.java
@@ -1,23 +1,29 @@
 /*
  * Copyright (c) 2009-2013 GreenVulcano ESB Open Source Project. All rights
  * reserved.
- * 
+ *
  * This file is part of GreenVulcano ESB.
- * 
+ *
  * GreenVulcano ESB is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * GreenVulcano ESB is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with GreenVulcano ESB. If not, see <http://www.gnu.org/licenses/>.
  */
 package it.greenvulcano.gvesb.core.flow.parallel;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+
+import org.apache.log4j.Logger;
 
 import it.greenvulcano.gvesb.buffer.GVBuffer;
 import it.greenvulcano.gvesb.core.flow.GVSubFlow;
@@ -28,17 +34,11 @@ import it.greenvulcano.log.GVLogger;
 import it.greenvulcano.log.NMDC;
 import it.greenvulcano.util.thread.ThreadMap;
 
-import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
-
-import org.apache.log4j.Logger;
-
 /**
- * 
+ *
  * @version 3.4.0 Jun 17, 2013
  * @author GreenVulcano Developer Team
- * 
+ *
  */
 public class SubFlowTask implements Callable<Result>
 {
@@ -46,11 +46,11 @@ public class SubFlowTask implements Callable<Result>
 
     private GVSubFlowPool       pool             = null;
     private GVSubFlow           subFlow          = null;
-    private GVBuffer            input;
-    private boolean             onDebug;
-    private boolean             changeLogContext;
+    private final GVBuffer            input;
+    private final boolean             onDebug;
+    private final boolean             changeLogContext;
     private Map<String, String> logContext;
-    private String              inputRefDP;
+    private final String              inputRefDP;
     private boolean             spawned           = false;
     private String              spawnedName       = null;
     private boolean             needsOutput       = true;
@@ -75,8 +75,16 @@ public class SubFlowTask implements Callable<Result>
         this.needsOutput = needsOutput;
     }
 
+    public boolean isSpawned() {
+        return this.spawned;
+    }
+
     public void setSpawned(boolean spawned) {
         this.spawned = spawned;
+    }
+
+    public String getSpawnedName() {
+        return this.spawnedName;
     }
 
     public void setSpawnedName(String spawnedName) {
@@ -87,60 +95,60 @@ public class SubFlowTask implements Callable<Result>
     public Result call() throws Exception {
         try {
             NMDC.push();
-            if (spawned && (spawnedName != null)) {
+            if (this.spawned && (this.spawnedName != null)) {
                 Thread th = Thread.currentThread();
                 String thn = th.getName();
                 thn = thn.substring(thn.lastIndexOf("_"));
-                th.setName(spawnedName + thn);
+                th.setName(this.spawnedName + thn);
             }
-            NMDC.setCurrentContext(logContext);
+            NMDC.setCurrentContext(this.logContext);
 
             Result result = null;
             GVSubFlow currSubFlow = null;
             try {
-                GVBuffer internalData = input;
+                GVBuffer internalData = this.input;
 
-                if (pool != null) {
-                    currSubFlow = pool.getSubFlow();
+                if (this.pool != null) {
+                    currSubFlow = this.pool.getSubFlow();
                 }
                 else {
-                    currSubFlow = subFlow;
+                    currSubFlow = this.subFlow;
                 }
 
-                if (changeLogContext) {
+                if (this.changeLogContext) {
                     NMDC.setOperation(currSubFlow.getFlowName());
                     GVBufferMDC.put(internalData);
                 }
-                
+
                 DataProviderManager dataProviderManager = DataProviderManager.instance();
-                if ((inputRefDP != null) && (inputRefDP.length() > 0)) {
-                    IDataProvider dataProvider = dataProviderManager.getDataProvider(inputRefDP);
+                if ((this.inputRefDP != null) && (this.inputRefDP.length() > 0)) {
+                    IDataProvider dataProvider = dataProviderManager.getDataProvider(this.inputRefDP);
                     try {
                         logger.debug("Working on Input data provider: " + dataProvider);
-                        internalData = new GVBuffer(input);
+                        internalData = new GVBuffer(this.input);
                         dataProvider.setObject(internalData);
                         Object inputCall = dataProvider.getResult();
                         internalData.setObject(inputCall);
                     }
                     finally {
-                        dataProviderManager.releaseDataProvider(inputRefDP, dataProvider);
+                        dataProviderManager.releaseDataProvider(this.inputRefDP, dataProvider);
                     }
                 }
 
-                GVBuffer output = currSubFlow.perform(internalData, onDebug);
-                if (needsOutput) {
-                	result = new Result(Result.State.STATE_OK, input, output);
+                GVBuffer output = currSubFlow.perform(internalData, this.onDebug);
+                if (this.needsOutput) {
+                	result = new Result(Result.State.STATE_OK, this.input, output);
                 }
                 else {
                 	result = new Result(Result.State.STATE_OK);
                 }
             }
             catch (InterruptedException exc) {
-                if (spawned) {
+                if (this.spawned) {
                     logger.error("SubFlow execution interrupted", exc);
                 }
-                if (needsOutput) {
-                    result = new Result(Result.State.STATE_INTERRUPTED, input, exc);
+                if (this.needsOutput) {
+                    result = new Result(Result.State.STATE_INTERRUPTED, this.input, exc);
                 }
                 else {
                     result = new Result(Result.State.STATE_INTERRUPTED, exc);
@@ -148,22 +156,22 @@ public class SubFlowTask implements Callable<Result>
                 Thread.currentThread().interrupt();
             }
             catch (Exception exc) {
-                if (spawned) {
+                if (this.spawned) {
                     logger.error("SubFlow execution failed", exc);
                 }
-                if (needsOutput) {
-                    result = new Result(Result.State.STATE_ERROR, input, exc);
+                if (this.needsOutput) {
+                    result = new Result(Result.State.STATE_ERROR, this.input, exc);
                 }
                 else {
                     result = new Result(Result.State.STATE_ERROR, exc);
                 }
             }
             finally {
-                if (pool != null) {
-                    pool.releaseSubFlow(currSubFlow);
+                if (this.pool != null) {
+                    this.pool.releaseSubFlow(currSubFlow);
                 }
-                if (subFlow != null) {
-                    subFlow.destroy();
+                if (this.subFlow != null) {
+                    this.subFlow.destroy();
                 }
             }
             return result;
@@ -171,7 +179,7 @@ public class SubFlowTask implements Callable<Result>
         finally {
             NMDC.pop();
             ThreadMap.clean();
-            
+
             this.pool = null;
             this.subFlow = null;
             //this.input = null;
@@ -180,22 +188,22 @@ public class SubFlowTask implements Callable<Result>
     }
 
     public Result getFailureResult(Throwable cause) {
-        if (needsOutput) {
-        	return new Result(Result.State.STATE_ERROR, input, cause);
+        if (this.needsOutput) {
+        	return new Result(Result.State.STATE_ERROR, this.input, cause);
         }
       	return new Result(Result.State.STATE_ERROR, cause);
     }
 
     public Result getTimeoutResult(InterruptedException cause) {
-        if (needsOutput) {
-        	return new Result(Result.State.STATE_ERROR, input, cause);
+        if (this.needsOutput) {
+        	return new Result(Result.State.STATE_ERROR, this.input, cause);
         }
        	return new Result(Result.State.STATE_ERROR, cause);
     }
 
     public Result getCancelledResult(CancellationException cause) {
-        if (needsOutput) {
-        	return new Result(Result.State.STATE_CANCELLED, input, cause);
+        if (this.needsOutput) {
+        	return new Result(Result.State.STATE_CANCELLED, this.input, cause);
         }
         return new Result(Result.State.STATE_CANCELLED, cause);
     }
