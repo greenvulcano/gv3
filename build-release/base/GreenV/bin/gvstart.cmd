8d6b3216fd78ade4db2f3a4f72f823cf7b36bf81
Gianluca <dmg75it@users.noreply.github.com>
2021-07-14 09:27:08 +0000

    Some libraries dependency change

--- build-release/base/GreenV/bin/gvstart.cmd
+++ build-release/base/GreenV/bin/gvstart.cmd
@@ -3,8 +3,8 @@
 FOR /F "tokens=*" %%B IN ('echo %~dp0') DO SET BASEDIR=%%B
 call "%BASEDIR%"\env_scripts.cmd
 
-set JB_OPTS=-Djboss.service.binding.set=%JBOSS_PORTSET% -Djboss.partition.name="%GV_SERVER_NAME%Partition" -Djboss.bind.address="%JBOSS_BIND_ADDRESS%" -Dbind.address="localhost" -Djava.rmi.server.hostname="localhost" -Djava.net.preferIPv4Stack="true" -Dit.greenvulcano.util.xpath.search.XPathAPIFactory.cfgFileXPath="GVCore.xml|/GVCore/GVXPath/XPath" -Djavax.xml.parsers.DocumentBuilderFactory="org.apache.xerces.jaxp.DocumentBuilderFactoryImpl" -Djavax.xml.transform.TransformerFactory="org.apache.xalan.processor.TransformerFactoryImpl" -Djxl.nogc=true -Dorg.apache.coyote.USE_CUSTOM_STATUS_MSG_IN_HEADER=true
-set GV_OPTS=-Dgv.app.home=/%GV_HOME% -Dgv.pop.uidcache.manager="it.greenvulcano.gvesb.virtual.pop.uidcache.MemoryUIDCacheManager" -Dit.greenvulcano.gvesb.identity.IdentityInfo.debug=true
+set JB_OPTS=-Djboss.service.binding.set=%JBOSS_PORTSET% -Djboss.partition.name="%GV_SERVER_NAME%Partition" -Djboss.bind.address="%JBOSS_BIND_ADDRESS%" -Dbind.address="localhost" -Djava.rmi.server.hostname="localhost" -Djava.net.preferIPv4Stack="true" -Djava.rmi.server.hostname="localhost" -Djxl.nogc=true -Dorg.apache.coyote.USE_CUSTOM_STATUS_MSG_IN_HEADER=true
+set GV_OPTS=-Dgv.app.home=/%GV_HOME% -Dit.greenvulcano.util.xpath.search.XPathAPIFactory.cfgFileXPath="GVCore.xml|/GVCore/GVXPath/XPath" -Djavax.xml.parsers.DocumentBuilderFactory="org.apache.xerces.jaxp.DocumentBuilderFactoryImpl" -Djavax.xml.transform.TransformerFactory="org.apache.xalan.processor.TransformerFactoryImpl" -Dgv.pop.uidcache.manager="it.greenvulcano.gvesb.virtual.pop.uidcache.MemoryUIDCacheManager" -Dit.greenvulcano.util.thread.BaseThread.dumpInstCount=true -Dit.greenvulcano.util.thread.BaseThread.dumpCreateStack=false -Dit.greenvulcano.gvesb.identity.IdentityInfo.debug=true
 set JAVA_OPTS=-Xms512M -Xmx1024M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled
 
 
