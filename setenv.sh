840eed2b4b070842bb5a21320fd8249b29ac3ae3
Gianluca Di Maio <g.dimaio@greenvulcano.com>
2023-03-06 07:47:26 +0100

    corrected compilation files

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-export GV_PROJECT_HOME=/home/gianluca/applicazioni/Test_JBOSS/GreenVulcanoOS
+export GV_PROJECT_HOME=/home/gianluca/lavoro/EAI/Eclipse-GreenVulcano-OS-git-3.4.0/gvesb-parent
 
-export PATH=$GV_PROJECT_HOME/tools/bin:$GV_PROJECT_HOME/tools/maven/bin:$PATH
+export JAVA_HOME=/opt/platform/jdk1.8.0
+export PATH=$JAVA_HOME/bin:$GV_PROJECT_HOME/tools/bin:$GV_PROJECT_HOME/tools/maven/bin:$PATH
